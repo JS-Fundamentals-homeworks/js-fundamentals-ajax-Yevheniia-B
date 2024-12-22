3bbc737da80f428ee5dce9e023aef7d34ef234c6
Yevheniia-B <evgenijar.work@gmail.com>
2024-12-22 23:58:20 +0200

    ajax

--- task1.js
+++ task1.js
@@ -4,4 +4,19 @@
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди node tests/task1.test.js 
+
+
+
+
+const usersList = document.querySelector('.usersList');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())  
+  .then(data => {
+    data.forEach(user => {
+      const li = document.createElement('li');
+      li.textContent = user.name; 
+      usersList.appendChild(li); 
+    });
+  });
--- task2.js
+++ task2.js
@@ -5,3 +5,38 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+
+const userNameInput = document.getElementById('userNameInput');
+const getUserButton = document.getElementById('getUserButton');
+const userCity = document.getElementById('userCity');
+
+function getUserByName(userName) {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      if (!response.ok) {
+        userCity.textContent = 'Error fetching data';
+        return;
+      }
+      return response.json();  
+    })
+    .then(data => {
+      if (data) {
+        const user = data.find(user => user.name.toLowerCase() === userName.toLowerCase());
+        if (user) {
+          userCity.textContent = `${user.address.city}`;
+        } else {
+          userCity.textContent = 'User not found';
+        }
+      }
+    });
+}
+
+getUserButton.addEventListener('click', () => {
+  const userName = userNameInput.value.trim(); 
+  if (userName) {
+    getUserByName(userName);
+  } else {
+    userCity.textContent = 'Please enter a user name'; 
+  }
+});
